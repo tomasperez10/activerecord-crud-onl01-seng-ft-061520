a6f93c421439eb69e310ea96b57603c9f3b5eba6
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 04:31:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   movie = Movie.create do |m|
     m.title = "Home Alone"
     m.release_date = 1990
