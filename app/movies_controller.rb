12730bedddedc9fb12622ed3465ef0aa9f9686f6
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 04:20:13 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,5 +97,6 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy
 end
